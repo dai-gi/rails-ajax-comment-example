a173ceafa24f033c6d0ac4b8066aba1beef64d4f
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-10-09 11:48:58 +0900

    Add: 削除機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :blogs do
-    resources :comments, only: [:create, :edit, :update]
+    resources :comments, only: [:create, :edit, :update, :destroy]
   end
 end
